9412309fa8f50a6e03bfb9a32ccb9775eb5383f0
fadilappu <fadilpachoos@gmail.com>
2023-10-05 13:12:33 +0530

    email js added

--- lib/pages/customtextfield.dart
+++ lib/pages/customtextfield.dart
@@ -34,19 +34,28 @@
 //   }
 // }
 import 'package:flutter/material.dart';
-Container CustTextField(String hintText, TextEditingController controller, context) {
+
+// final _formKey = GlobalKey<FormState>();
+
+Container CustTextField(String hintText, controller, context) {
   return Container(
     width: MediaQuery.of(context).size.width / 4,
     height: 40,
     child: TextFormField(
-      controller: controller, // Pass the controller
+      controller: controller,
       keyboardType: TextInputType.phone,
-      style: TextStyle( // Set the text color to white
-        color:Colors.white,
+      // validator: (value) {
+      //   if (value == null || value.isEmpty) {
+      //     return '*Required';
+      //   }
+      //   return null;
+      // },
+      style: TextStyle(
+        color: Colors.white,
         fontSize: 15,
       ),
       decoration: InputDecoration(
-        hintText: hintText, // Pass the hint text
+        hintText: hintText,
         hintStyle: TextStyle(
           color: Colors.white,
           fontSize: 15,
@@ -67,4 +76,3 @@ Container CustTextField(String hintText, TextEditingController controller, conte
     ),
   );
 }
-
