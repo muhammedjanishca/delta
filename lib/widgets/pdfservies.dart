6f0d2129799acfad4dd87b81d6feb4bbf4e426cc
fadilappu <fadilpachoos@gmail.com>
2023-11-19 11:10:37 +0530

    pdf change success

--- lib/widgets/pdfservies.dart
+++ lib/widgets/pdfservies.dart
@@ -7,13 +7,12 @@ import 'package:syncfusion_flutter_pdf/pdf.dart';
 import '../save_file_mobile.dart' if (dart.library.html) 'save_file_web.dart';
 
 class PdfService {
-  Future<void> generateInvoice(List cartItems,String current_address,last_address) async {
-    // cartItems);
+  Future<void> generateInvoice(
+      List cartItems, String current_address, last_address) async {
     //Create a PDF document.
     final PdfDocument document = PdfDocument();
     //Add page to the PDF
     final PdfPage page = document.pages.add();
-    
     //Get page client size
     final Size pageSize = page.getClientSize();
     //Draw rectangle
@@ -23,14 +22,12 @@ class PdfService {
     //Generate PDF grid.
     final PdfGrid grid = getGrid(cartItems);
     //Draw the header section by creating text element
-     PdfLayoutResult result = drawHeader(page, pageSize, grid,current_address,last_address);
-      // drawFooter(page, pageSize,grid,result);
+    PdfLayoutResult result =
+    drawHeader(page, pageSize, grid, current_address, last_address);
     //Draw grid
     drawGrid(page, grid, result);
     //Add invoice footer
-    //  final PdfPage page2 = document.pages.add();
-    drawFooter(grid, result, page,document);
-  
+    drawFooter(grid, result, page, document);
     //Save the PDF document
     final List<int> bytes = document.saveSync();
     //Dispose the document.
@@ -41,7 +38,9 @@ class PdfService {
 }
 
 //Draws the invoice header
-PdfLayoutResult drawHeader(PdfPage page, Size pageSize, PdfGrid grid,String current_address,last_address) {
+PdfLayoutResult drawHeader(PdfPage page, Size pageSize, PdfGrid grid,
+    String current_address, last_address) {
+      int invoiceCounter = 1;
   //Draw rectangle
   page.graphics.drawRectangle(
       brush: PdfSolidBrush(PdfColor(68, 114, 196)),
@@ -57,7 +56,7 @@ PdfLayoutResult drawHeader(PdfPage page, Size pageSize, PdfGrid grid,String curr
       bounds: Rect.fromLTWH(400, 0, pageSize.width - 400, 90),
       brush: PdfSolidBrush(PdfColor(68, 114, 196)));
 
-  page.graphics.drawString(r'$' + getTotalAmount(grid).toString(),
+  page.graphics.drawString(r'$' + getGrandTotal(grid).toString(),
       PdfStandardFont(PdfFontFamily.helvetica, 18),
       bounds: Rect.fromLTWH(400, 0, pageSize.width - 400, 100),
       brush: PdfBrushes.white,
@@ -76,161 +75,57 @@ PdfLayoutResult drawHeader(PdfPage page, Size pageSize, PdfGrid grid,String curr
   //Create data foramt and convert it to text.
   final DateFormat format = DateFormat.yMMMMd('en_US');
   final String invoiceNumber =
-      'Invoice Number: 2058557939\r\n\r\nDate: ${format.format(DateTime.now())}';
+      'Invoice Number: 1234567890\r\n\r\nDate: ${format.format(DateTime.now())}';
   final Size contentSize = contentFont.measureString(invoiceNumber);
 
-
-
-  
   // ignore: leading_newlines_in_multiline_strings
- String address = current_address;
+  // String address = current_address;
 
   PdfTextElement(text: invoiceNumber, font: contentFont).draw(
       page: page,
       bounds: Rect.fromLTWH(pageSize.width - (contentSize.width + 30), 120,
           contentSize.width + 30, pageSize.height - 120));
 
-  return PdfTextElement(text:' ${last_address['COMPANY NAME']} \n${last_address['Contact Number']}\n${last_address['Street Address']}\n${last_address['city']} ', font: contentFont).draw(
-      page: page,
-      bounds: Rect.fromLTWH(30, 120, pageSize.width - (contentSize.width + 30),
-          pageSize.height - 120))!;
+  return PdfTextElement(
+          text:
+              ' ${last_address['COMPANY NAME']} \n${last_address['Contact Number']}\n${last_address['Street Address']}\n${last_address['city']} ',
+          font: contentFont)
+      .draw(
+          page: page,
+          bounds: Rect.fromLTWH(
+              30,
+              120,
+              pageSize.width - (contentSize.width + 30),
+              pageSize.height - 120))!;
 }
 
 //Draws the grid
 void drawGrid(PdfPage page, PdfGrid grid, PdfLayoutResult result) {
-  Rect? totalPriceCellBounds;
-  Rect? quantityCellBounds;
-  //Invoke the beginCellLayout event.
-  // grid.beginCellLayout = (Object sender, PdfGridBeginCellLayoutArgs args) {
-  //   final PdfGrid grid = sender as PdfGrid;
-  //   if (args.cellIndex == grid.columns.count - 1) {
-  //     totalPriceCellBounds = args.bounds;
-  //   } else if (args.cellIndex == grid.columns.count - 2) {
-  //     quantityCellBounds = args.bounds;
-  //   }
-  // };
-  // grid.endCellLayout =(sender, args) {
-  // //  final PdfGrid grid = sender as PdfGrid;
-    
-  //   final PdfGrid grid = sender as PdfGrid;
-  //   if (args.cellIndex == grid.columns.count - 1) {
-  //     totalPriceCellBounds = args.bounds;
-  //   } else if (args.cellIndex == grid.columns.count - 2) {
-  //     quantityCellBounds = args.bounds;
-  //   }
-    
-  // };
-  //Draw the PDF grid and get the result.
   result = grid.draw(
       page: page, bounds: Rect.fromLTWH(0, result.bounds.bottom + 40, 0, 0))!;
-      // drawFooter(grid, result,page);
-
-  //Draw grand total.
-  // grid.endCellLayout = (Object sender, PdfGridEndCellLayoutArgs args) {
-
-  
-//     page.graphics.drawString('Grand Total'+getTotalAmount(grid).toString(),
-//       PdfStandardFont(PdfFontFamily.helvetica, 9, style: PdfFontStyle.bold),
-//       bounds: Rect.fromLTWH(quantityCellBounds!.left, result.bounds.bottom + 10,
-//           quantityCellBounds!.width, quantityCellBounds!.height));
-  
- 
-// };
-
-  // page.graphics.drawString(getTotalAmount(grid).toString(),
-  //     PdfStandardFont(PdfFontFamily.helvetica, 9, style: PdfFontStyle.bold),
-  //     bounds: Rect.fromLTWH(
-  //         totalPriceCellBounds!.left,
-  //         result.bounds.bottom + 10,
-  //         totalPriceCellBounds!.width,
-  //         totalPriceCellBounds!.height));
 }
 
 //Draw the invoice footer data.
- PdfLayoutResult drawFooter(PdfGrid grid,PdfLayoutResult result,PdfPage page,PdfDocument document) {
-  
-  // grid.endCellLayout =(sender, args) {
-  //  final PdfGrid grid = sender as PdfGrid;
-   for(int i=0;i<document.pages.count;i++){
-    if(i==document.pages.count-1){
-          return PdfTextElement(text: 'Grand Totalasssss'+getTotalAmount(grid).toString()
-    
-    ).draw(
-    page:document.pages[i],
-    bounds: Rect.fromLTWH(0,0,0,0)
-      )!
-
-    ;
+PdfLayoutResult drawFooter(
+    PdfGrid grid, PdfLayoutResult result, PdfPage page, PdfDocument document) {
+  for (int i = 0; i < document.pages.count; i++) {
+    if (i == document.pages.count - 1) {
+      return PdfTextElement(
+              text: '''800 Interchange Blvd.\r\n\r\nSuite 2501, Austin,
+          TX 78721\r\n\r\nAny Questions? sales@deltanationals.com''',
+              font: PdfStandardFont(PdfFontFamily.timesRoman, 11))
+          .draw(page: document.pages[i], bounds: Rect.fromLTWH(20, 670, 0, 0))!;
     }
-   }
-   
-      //  return PdfTextElement(text:' ${last_address['COMPANY NAME']} \n${last_address['Contact Number']}\n${last_address['Street Address']}\n${last_address['city']} ', font: contentFont).draw(
-      // page: page,
-      // bounds: Rect.fromLTWH(30, 120, pageSize.width - (contentSize.width + 30),
-      //     pageSize.height - 120))!;
-      // if(grid.endPageLayout)
-     
-     
-      
-      // 'Grand Totalasssss'+getTotalAmount(grid).toString(), PdfStandardFont(PdfFontFamily.helvetica, 9),
-      // format: PdfStringFormat(alignment: PdfTextAlignment.right),
-      // bounds: Rect.fromLTWH(pageSize.width - 30,pageSize.height - 70, 0, 0));
-
-  // };
-  return PdfTextElement(text: "").draw()! ;
-  
-  // final PdfPen linePen =
-  //     PdfPen(PdfColor(142, 170, 219), dashStyle: PdfDashStyle.custom);
-  // linePen.dashPattern = <double>[3, 3];
-  // //Draw line
-  // page.graphics.drawLine(linePen, Offset(0, pageSize.height - 100),
-  //     Offset(pageSize.width, pageSize.height - 100));
-
-  // const String footerContent =
-  //     // ignore: leading_newlines_in_multiline_strings
-  //     '''800 Interchange Blvd.\r\n\r\nSuite 2501, Austin,
-  //        TX 78721\r\n\r\nAny Questions? support@adventure-works.com''';
-
-  // //Added 30 as a margin for the layout
-  // page.graphics.drawString(
-  //    footerContent, PdfStandardFont(PdfFontFamily.helvetica, 9),
-  //     format: PdfStringFormat(alignment: PdfTextAlignment.right),
-  //     bounds: Rect.fromLTWH(pageSize.width - 30,pageSize.height - 70, 0, 0));
-}
-void lastpage(PdfPage page, Size pageSize,PdfGrid grid, PdfDocument document,PdfLayoutResult result){
-//  if( page.defaultLayerIndex==page.-1)
-// if()
- page.graphics.drawString(
-      'Grand Totalasssss'+getTotalAmount(grid).toString(), PdfStandardFont(PdfFontFamily.helvetica, 9),
-      format: PdfStringFormat(alignment: PdfTextAlignment.right),
-      bounds: Rect.fromLTWH(pageSize.width - 30,pageSize.height - 70, 0, 0));
-}
-
-  // Function to create a custom footer
-  PdfPageTemplateElement createCustomFooter() {
-    PdfPageTemplateElement footer = PdfPageTemplateElement(
-        Rect.fromLTWH(0, 0, PdfPageSize.letter.width, 100));
-
-    // Add custom footer content
-    String footerContent = '''800 Interchange Blvd.\r\n\r\nSuite 2501, Austin,
-         TX 78721\r\n\r\nAny Questions? support@adventure-works.com''';
-
-    PdfFont font = PdfStandardFont(PdfFontFamily.timesRoman, 11);
-    footer.graphics.drawString(footerContent, font,
-        bounds: Rect.fromLTWH(0, 0, PdfPageSize.letter.width, 100));
-
-    return footer;
   }
+  return PdfTextElement(text: "").draw()!;
+}
 
 //Create PDF grid and return
-    PdfGrid getGrid(List cartItems) {
-  //Create a PDF grid
+PdfGrid getGrid(List cartItems) {
   final PdfGrid grid = PdfGrid();
-  //Secify the columns count to the grid.
   grid.columns.add(count: 7);
-  //Create the header row of the grid.
+
   final PdfGridRow headerRow = grid.headers.add(1)[0];
-  //Set style
   headerRow.style.backgroundBrush = PdfSolidBrush(PdfColor(68, 114, 196));
   headerRow.style.textBrush = PdfBrushes.white;
   headerRow.cells[0].value = "SINO";
@@ -240,54 +135,93 @@ void lastpage(PdfPage page, Size pageSize,PdfGrid grid, PdfDocument document,Pdf
   headerRow.cells[4].value = "Unit";
   headerRow.cells[5].value = "Price";
   headerRow.cells[6].value = "Total";
-  //Add rows
+  headerRow.height = 35;
+  for (int i = 0; i < headerRow.cells.count; i++) {
+    headerRow.cells[i].style.stringFormat = PdfStringFormat(
+        alignment: PdfTextAlignment.center,
+        lineAlignment: PdfVerticalAlignment.middle);
+  }
+
+  double grandTotal = 0;
+
   for (int i = 0; i < cartItems.length; i++) {
     final item = jsonDecode(cartItems[i]);
-    
     final PdfGridRow row = grid.rows.add();
-
-    row.cells[0].value = (i + 1).toString(); // SINO
-    row.cells[1].value = item['productName']; // Description
-    row.cells[2].value = item['productCode']; // Product Code
-    row.cells[3].value = item['quantity'].toString(); // Quantity
-    row.cells[4].value = "Unit"; // Unit (you can replace with the actual unit)
-    row.cells[5].value = item['price'].toStringAsFixed(2); // Price
-    row.cells[6].value =
-        (item['price'] * item['quantity']).toStringAsFixed(2); // Total
-  }
-  
-  //Apply the table built-in style
-  grid.applyBuiltInStyle(PdfGridBuiltInStyle.listTable4Accent5);
-  //Set gird columns width
-  grid.columns[1].width = 200;
-  for (int i = 0; i < headerRow.cells.count; i++) {
-    headerRow.cells[i].style.cellPadding =
-        PdfPaddings(bottom: 5, left: 5, right: 5, top: 5);
-  }
-  for (int i = 0; i < grid.rows.count; i++) {
-    final PdfGridRow row = grid.rows[i];
+    row.cells[0].value = (i + 1).toString();
+    row.cells[1].value = item['productName'];
+    row.cells[2].value = item['productCode'];
+    row.cells[3].value = item['quantity'].toString();
+    row.cells[4].value = "Unit";
+    row.cells[5].value = item['price'].toStringAsFixed(2);
+    row.cells[6].value = (item['price'] * item['quantity']).toStringAsFixed(2);
+    row.height = 30;
     for (int j = 0; j < row.cells.count; j++) {
-      final PdfGridCell cell = row.cells[j];
-      if (j == 0) {
-        cell.stringFormat.alignment = PdfTextAlignment.center;
-      }
-      cell.style.cellPadding =
-          PdfPaddings(bottom: 5, left: 5, right: 5, top: 5);
+      row.cells[j].style.stringFormat = PdfStringFormat(
+          alignment: PdfTextAlignment.center,
+          lineAlignment: PdfVerticalAlignment.middle);
     }
+    grandTotal += double.parse(row.cells[6].value.toString());
   }
+
+  final PdfGridRow grandTotalRow = grid.rows.add();
+  grandTotalRow.cells[0].value = '';
+  grandTotalRow.cells[1].value = '';
+  grandTotalRow.cells[2].value = '';
+  grandTotalRow.cells[3].value = '';
+  grandTotalRow.cells[4].value = '';
+  grandTotalRow.cells[5].value = 'Net before VAT :';
+  grandTotalRow.cells[6].value = grandTotal.toStringAsFixed(2);
+  grandTotalRow.height = 30;
+  for (int i = 0; i < grandTotalRow.cells.count; i++) {
+    grandTotalRow.cells[i].style.stringFormat = PdfStringFormat(
+        alignment: PdfTextAlignment.center,
+        lineAlignment: PdfVerticalAlignment.middle);
+  }
+
+  final PdfGridRow vatRow = grid.rows.add();
+  vatRow.cells[0].value = '';
+  vatRow.cells[1].value = '';
+  vatRow.cells[2].value = '';
+  vatRow.cells[3].value = '';
+  vatRow.cells[4].value = '';
+  vatRow.cells[5].value = 'VAT (15%) :';
+  vatRow.cells[6].value = (grandTotal * 0.15).toStringAsFixed(2);
+  vatRow.height = 30;
+  for (int i = 0; i < vatRow.cells.count; i++) {
+    vatRow.cells[i].style.stringFormat = PdfStringFormat(
+        alignment: PdfTextAlignment.center,
+        lineAlignment: PdfVerticalAlignment.middle);
+  }
+
+  final PdfGridRow totalRow = grid.rows.add();
+  totalRow.cells[0].value = '';
+  totalRow.cells[1].value = '';
+  totalRow.cells[2].value = '';
+  totalRow.cells[3].value = '';
+  totalRow.cells[4].value = '';
+  totalRow.cells[5].value = 'Grand Total :';
+  totalRow.cells[6].value =
+      (grandTotal + (grandTotal * 0.15)).toStringAsFixed(2);
+  totalRow.height = 30;
+  for (int i = 0; i < totalRow.cells.count; i++) {
+    totalRow.cells[i].style.stringFormat = PdfStringFormat(
+        alignment: PdfTextAlignment.center,
+        lineAlignment: PdfVerticalAlignment.middle);
+  }
+
+  grid.applyBuiltInStyle(PdfGridBuiltInStyle.listTable4Accent5);
+  grid.columns[1].width = 200;
 
   return grid;
 }
 
-//Create and row for the grid.
-
 //Get the total amount.
-double getTotalAmount(PdfGrid grid) {
+String getGrandTotal(PdfGrid grid) {
   double total = 0;
-  for (int i = 0; i < grid.rows.count; i++) {
+  for (int i = 0; i < grid.rows.count - 3; i++) {
     final String value =
         grid.rows[i].cells[grid.columns.count - 1].value as String;
     total += double.parse(value);
   }
-  return total;
+  return (total + total * 0.15).toStringAsFixed(2);
 }
