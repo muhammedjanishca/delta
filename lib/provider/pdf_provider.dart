b622a6f31c89d3516d5eb62f681479882d703e1e
shihanhazzan <shihanhazzan@gmail.com>
2023-11-29 12:22:57 +0530

    testing stash agin

--- lib/provider/pdf_provider.dart
+++ lib/provider/pdf_provider.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 
 class SelectedPriceProvider extends ChangeNotifier {
   bool checkPrCode = false;
-//pdf provider used set pdf for download
+
 
   void setCheckPrCode(bool value) {
     checkPrCode = value;
