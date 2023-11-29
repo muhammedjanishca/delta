91045d69b80cc4cf88ca3e7856e37ac78e63537f
shihanhazzan <shihanhazzan@gmail.com>
2023-11-29 16:10:21 +0530

    index html,quote page trans delta address

--- lib/provider/pdf_provider.dart
+++ lib/provider/pdf_provider.dart
@@ -4,7 +4,6 @@ import 'package:provider/provider.dart';
 class SelectedPriceProvider extends ChangeNotifier {
   bool checkPrCode = false;
 
-
   void setCheckPrCode(bool value) {
     checkPrCode = value;
     notifyListeners();
